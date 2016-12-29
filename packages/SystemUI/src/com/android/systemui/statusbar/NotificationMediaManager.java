207b1bba9a43c6356029e6d8abbf6566c1967184
ezio84 <brabus84@gmail.com>
2016-12-29 01:07:25 +0100

    Lockscreen album art: Allow to configure or disable lockscreen media art blur [1/2]
    * Adapt to existing crDroid customizations
    
    Change-Id: I68895ae6a471bbc99b538b85337e15e8793665ce

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -36,6 +36,7 @@ import android.media.session.MediaSession;
 import android.media.session.PlaybackState;
 import android.os.AsyncTask;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.NotificationStats;
@@ -106,6 +107,8 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
             "lineagesecure:" + LineageSettings.Secure.LOCKSCREEN_MEDIA_METADATA;
     private static final String LOCKSCREEN_ALBUMART_FILTER =
             "system:" + Settings.System.LOCKSCREEN_ALBUMART_FILTER;
+    private static final String LOCKSCREEN_MEDIA_BLUR =
+            "system:" + Settings.System.LOCKSCREEN_MEDIA_BLUR;
 
     private static final String NOWPLAYING_SERVICE = "com.google.android.as";
     private final StatusBarStateController mStatusBarStateController
@@ -146,6 +149,7 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
     private final Lazy<Optional<StatusBar>> mStatusBarOptionalLazy;
     private final MediaArtworkProcessor mMediaArtworkProcessor;
     private final Set<AsyncTask<?, ?, ?>> mProcessArtworkTasks = new ArraySet<>();
+    private float mLockscreenMediaBlur;
 
     protected NotificationPresenter mPresenter;
     private MediaController mMediaController;
@@ -705,6 +709,9 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
     private void finishUpdateMediaMetaData(boolean metaDataChanged, boolean allowEnterAnimation,
             @Nullable Bitmap bmp) {
         Drawable artworkDrawable = null;
+        mLockscreenMediaBlur = (float) Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_MEDIA_BLUR, 25,
+                UserHandle.USER_CURRENT);
         // set media artwork as lockscreen wallpaper if player is playing
         if (bmp != null && (mShowMediaMetadata || !ENABLE_LOCKSCREEN_WALLPAPER) &&
                 PlaybackState.STATE_PLAYING == getMediaControllerPlaybackState(mMediaController)) {
@@ -724,11 +731,11 @@ public class NotificationMediaManager implements Dumpable, TunerService.Tunable 
                     break;
                 case 3:
                     artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(),
-                        ImageHelper.getBlurredImage(mContext, bmp, 7.0f));
+                        ImageHelper.getBlurredImage(mContext, bmp, mLockscreenMediaBlur));
                     break;
                 case 4:
                     artworkDrawable = new BitmapDrawable(mBackdropBack.getResources(),
-                        ImageHelper.getGrayscaleBlurredImage(mContext, bmp, 7.0f));
+                        ImageHelper.getGrayscaleBlurredImage(mContext, bmp, mLockscreenMediaBlur));
                     break;
             }
         }
