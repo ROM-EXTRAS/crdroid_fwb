cbf433a4d9956c1d6830b510c0fb02462c6e33fd
Gaoxiang Chen <gaochen@codeaurora.org>
2017-06-06 18:29:12 +0800

    Camera: Skip stream size check for whitelisted apps..
    Issue:
    For quadracfa capture, Blob/YUV output streams need to be
    configured with custom dimensions which will not be
    available in advertised stream configurations map.
    
    Fix:
    Skip the stream size check for whitelisted apps to allow
    configuration of streams with custom dimensions.
    
    Also, additionally, remove session id check so that
    buffers from one session can be passed on to another
    session for reprocess.
    
    Setprop to be used:
    adb shell setprop persist.vendor.camera.privapp.list <pack1,pack2>
    
    CRs-Fixed: 2075934
    Change-Id: Ie9c950fdc4e1675d19c38630d7063ac9d7c9d5b1
    (cherry picked from commit 6d1de963ca530a3fdf307962824ebc794943b13f)
    
    Issue:
    For quadracfa capture, Blob/YUV output streams need to be
    configured with custom dimensions which will not be
    available in advertised stream configurations map.
    
    Fix:
    Skip the stream size check for whitelisted apps to allow
    configuration of streams with custom dimensions.
    
    Also, additionally, remove session id check so that
    buffers from one session can be passed on to another
    session for reprocess.
    
    Setprop to be used:
    adb shell setprop persist.vendor.camera.privapp.list <pack1,pack2>
    
    CRs-Fixed: 2075934
    Change-Id: Ie9c950fdc4e1675d19c38630d7063ac9d7c9d5b1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
+++ core/java/android/hardware/camera2/impl/CameraDeviceImpl.java
@@ -16,10 +16,13 @@
 
 package android.hardware.camera2.impl;
 
+import static android.hardware.camera2.CameraAccessException.CAMERA_IN_USE;
+
 import static com.android.internal.util.function.pooled.PooledLambda.obtainRunnable;
 
 import android.annotation.NonNull;
 import android.content.Context;
+import android.app.ActivityThread;
 import android.graphics.ImageFormat;
 import android.hardware.ICameraService;
 import android.app.ActivityThread;
@@ -1574,7 +1577,6 @@ public class CameraDeviceImpl extends CameraDevice
                 Log.w(TAG, "ignore input format/size check for white listed app");
                 return;
             }
-
             if (!checkInputConfigurationWithStreamConfigurations(inputConfig, /*maxRes*/false) &&
                     !checkInputConfigurationWithStreamConfigurations(inputConfig, /*maxRes*/true)) {
                 throw new IllegalArgumentException("Input config with format " +
