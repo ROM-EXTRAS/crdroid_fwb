a91f17c55a73e011bf1041424a1efe7c78dae79f
LibXZR <i@xzr.moe>
2021-09-01 17:38:50 +0800

    Introduce Optimized Charge service
    This is a simple but useful feature that protects battery from
    overcharge. Many tests have confirmed that charging lithium batteries
    to 80% or more hurts their capabilities. Also this feature has
    implemented by many oems. Its time to have my own implement for it.
    
     ** AICP Edits: added SystemSetting validators
    
    Change-Id: I54175e2501a45bb66f95fe9f295fe1b822ede701
    Signed-off-by: LibXZR <i@xzr.moe>

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -16,6 +16,7 @@
 
 package android.provider.settings.validators;
 
+import static android.provider.settings.validators.SettingsValidators.ANY_INTEGER_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.ANY_STRING_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.BOOLEAN_VALIDATOR;
 import static android.provider.settings.validators.SettingsValidators.COMPONENT_NAME_VALIDATOR;
@@ -203,5 +204,8 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.SHOW_BATTERY_PERCENT, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.ACCELEROMETER_ROTATION_ANGLES, NON_NEGATIVE_INTEGER_VALIDATOR);
+        VALIDATORS.put(System.OPTIMIZED_CHARGE_ENABLED, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(System.OPTIMIZED_CHARGE_CEILING, ANY_INTEGER_VALIDATOR);
+        VALIDATORS.put(System.OPTIMIZED_CHARGE_FLOOR, ANY_INTEGER_VALIDATOR);
     }
 }
