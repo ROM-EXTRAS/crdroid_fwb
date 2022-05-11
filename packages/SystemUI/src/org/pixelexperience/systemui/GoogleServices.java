90b89339073a92a0e44bd169a06e4e1f8a237c70
DroidFreak32 <rushabshah32@gmail.com>
2022-05-12 06:52:17 +1000

    Revert "SystemUIGoogle: Don't start FaceNotificationService if using custom face impl"
    This reverts commit 63668904e3356529b6591e3b4545ca7e90de26dc.
    
    Change-Id: I769fd1e06ea1653a85dbdf3c2598f15e85e3a45c

--- packages/SystemUI/src/org/pixelexperience/systemui/GoogleServices.java
+++ packages/SystemUI/src/org/pixelexperience/systemui/GoogleServices.java
@@ -38,8 +38,6 @@ import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
 import com.google.android.systemui.face.FaceNotificationService;
 import com.google.android.systemui.input.TouchContextService;
 
-import com.android.internal.util.custom.faceunlock.FaceUnlockUtils;
-
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -84,8 +82,7 @@ public class GoogleServices extends VendorServices {
         if (new ColumbusContext(mContext).isAvailable()) {
             addService(mColumbusServiceLazy.get());
         }
-        if (!FaceUnlockUtils.isFaceUnlockSupported() &&
-                mContext.getPackageManager().hasSystemFeature("android.hardware.biometrics.face")) {
+        if (mContext.getPackageManager().hasSystemFeature("android.hardware.biometrics.face")) {
             addService(new FaceNotificationService(mContext));
         }
         if (mContext.getResources().getBoolean(R.bool.config_touch_context_enabled)) {
