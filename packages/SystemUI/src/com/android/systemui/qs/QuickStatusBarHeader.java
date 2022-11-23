46fec41f1bca289a0bcc8df56327fff9d7ce2362
DroidFreak32 <rushabshah32@gmail.com>
2022-11-24 01:10:39 +0530

    FIXUP: a2a2c358d1d8
    Change I77e8104b31932b056e250a1634f50e1323087d10
    
    Change-Id: If827f7c4550403ab3348921f6e757c61412d2fd9

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -141,7 +141,6 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     private boolean mShowClock;
     private boolean mShowDate;
 
-    private final ActivityStarter mActivityStarter;
     private final Vibrator mVibrator;
 
     private int mStatusBarBatteryStyle, mQSBatteryStyle, mQSBatteryLocation;
