crdroidandroid/android_frameworks_base

base: allow disable of screenshot shutter sound [1/2]
Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

core/java/android/provider/Settings.java
@@ -5539,6 +5539,12 @@ public final class Settings {
         public static final String ANBI_ENABLED = "anbi_enabled";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
          * Three Finger Gesture
          * @hide
          */

ROM-EXTRAS/crdroid_fwb

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/android/provider/Settings.java
@@ -5885,6 +5885,12 @@ public final class Settings {
         public static final String QS_TILE_ANIMATION_INTERPOLATOR = "qs_tile_animation_interpolator";
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *