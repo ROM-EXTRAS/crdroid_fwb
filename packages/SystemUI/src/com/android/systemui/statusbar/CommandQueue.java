crdroidandroid/android_frameworks_base

Pulse initial checkin for Android 12 [1/2]
@neobuddy89:
* This includes few of NPE fixes and improvements.
* Android 12 removed colored notifications in ff165814e10cf757981d1e0254bd88473dff126e,
  so retire pulse color based on album art till we find other way.

----
ezio:

big squash here to handle Android 11 changes,
check external pulse tree to know who contributed
to this epic feature that Randall made back in the days
(yeah, we're getting old) :D

some mentions: neobuddy89, Alex Cruz, LorDClockaN, varund7726

ps: now when the the new qs media player notification is enabled,
a different call to media processor is done by the mediadatamanager
in packages/SystemUI/src/com/android/systemui/media/, that's why the
need of MediaDataManager.Listener in NotificationMediaManager.
reminder for myself:  we can acces properties of kotlin "data" classes
like MediaData.kt just calling a built-in getter (like getBackgroundColor
for the backgroundColor prop). For reference, it's also possible to call
straihgt mediaData.background color but only after adding the @JvmField
flag to the prop declaration
----

Change-Id: I0205f8ba6b8883012d1b809de9a33f6391c4cf0c
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -147,6 +147,8 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
     private static final int MSG_EMERGENCY_ACTION_LAUNCH_GESTURE      = 58 << MSG_SHIFT;
     private static final int MSG_SET_NAVIGATION_BAR_LUMA_SAMPLING_ENABLED = 59 << MSG_SHIFT;
     private static final int MSG_SET_UDFPS_HBM_LISTENER = 60 << MSG_SHIFT;
+    private static final int MSG_SCREEN_PINNING_STATE_CHANGED      = 61 << MSG_SHIFT;
+    private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED   = 62 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -400,6 +402,10 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
          * @see IStatusBar#setNavigationBarLumaSamplingEnabled(int, boolean)
          */
         default void setNavigationBarLumaSamplingEnabled(int displayId, boolean enable) {}
+
+        default void screenPinningStateChanged(boolean enabled) {}
+
+        default void leftInLandscapeChanged(boolean isLeft) {}
     }
 
     public CommandQueue(Context context) {
@@ -1104,6 +1110,24 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
         GcUtils.runGcAndFinalizersSync();
     }
 
+    @Override
+    public void screenPinningStateChanged(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SCREEN_PINNING_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_SCREEN_PINNING_STATE_CHANGED,
+                    enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
+    @Override
+    public void leftInLandscapeChanged(boolean isLeft) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED,
+                    isLeft ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1475,6 +1499,16 @@ public class CommandQueue extends IStatusBar.Stub implements CallbackController<
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).setNavigationBarLumaSamplingEnabled(msg.arg1,
                                 msg.arg2 != 0);
+                    }
+                    break;
+                case MSG_SCREEN_PINNING_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
                     }
                     break;
             }

ROM-EXTRAS/crdroid_fwb

packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -150,6 +150,8 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_SET_NAVIGATION_BAR_LUMA_SAMPLING_ENABLED = 59 << MSG_SHIFT;
     private static final int MSG_SET_UDFPS_HBM_LISTENER = 60 << MSG_SHIFT;
     private static final int MSG_TOGGLE_CAMERA_FLASH               = 61 << MSG_SHIFT;
+    private static final int MSG_SCREEN_PINNING_STATE_CHANGED      = 62 << MSG_SHIFT;
+    private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED   = 63 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -414,6 +416,10 @@ public class CommandQueue extends IStatusBar.Stub implements
         default void setNavigationBarLumaSamplingEnabled(int displayId, boolean enable) {}
 
         default void toggleCameraFlash() { }
+
+        default void screenPinningStateChanged(boolean enabled) {}
+
+        default void leftInLandscapeChanged(boolean isLeft) {}
     }
 
     public CommandQueue(Context context) {
@@ -1132,6 +1138,24 @@ public class CommandQueue extends IStatusBar.Stub implements
         }
     }
 
+    @Override
+    public void screenPinningStateChanged(boolean enabled) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_SCREEN_PINNING_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_SCREEN_PINNING_STATE_CHANGED,
+                    enabled ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
+    @Override
+    public void leftInLandscapeChanged(boolean isLeft) {
+        synchronized (mLock) {
+            mHandler.removeMessages(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED);
+            mHandler.obtainMessage(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED,
+                    isLeft ? 1 : 0, 0, null).sendToTarget();
+        }
+    }
+
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -1510,6 +1534,16 @@ public class CommandQueue extends IStatusBar.Stub implements
                 case MSG_TOGGLE_CAMERA_FLASH:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).toggleCameraFlash();
+                    }
+                    break;
+                case MSG_SCREEN_PINNING_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
+                    }
+                    break;
+                case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
+                    for (int i = 0; i < mCallbacks.size(); i++) {
+                        mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
                     }
                     break;
             }