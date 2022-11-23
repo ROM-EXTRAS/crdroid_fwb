98d549a5c2b61c2fa79d01a2e569b6d7f8c0cf04
DroidFreak32 <rushabshah32@gmail.com>
2022-11-24 00:40:47 +0530

    Revert "Allow toggling camera/mic privacy indicator [1/2]"
    This reverts commit c56e2c15365b260fc6505300f91178f3cb2a841a.
    
    Change-Id: I286f0817dea1ed7a61c17aed56938788eeb1537a

--- core/java/android/permission/PermissionUsageHelper.java
+++ core/java/android/permission/PermissionUsageHelper.java
@@ -69,6 +69,9 @@ import java.util.Objects;
 public class PermissionUsageHelper implements AppOpsManager.OnOpActiveChangedListener,
         AppOpsManager.OnOpStartedListener {
 
+    /** Whether to show the mic and camera icons.  */
+    private static final String PROPERTY_CAMERA_MIC_ICONS_ENABLED = "camera_mic_icons_enabled";
+
     /** Whether to show the Permissions Hub.  */
     private static final String PROPERTY_PERMISSIONS_HUB_2_ENABLED = "permissions_hub_2_enabled";
 
@@ -88,15 +91,9 @@ public class PermissionUsageHelper implements AppOpsManager.OnOpActiveChangedLis
                 PROPERTY_PERMISSIONS_HUB_2_ENABLED, false);
     }
 
-    private boolean shouldShowIndicators() {
-        return shouldShowCameraIndicator() || shouldShowLocationIndicator() ||
-                shouldShowPermissionsHub();
-    }
-
-    private boolean shouldShowCameraIndicator() {
-        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
-            Settings.Secure.ENABLE_CAMERA_PRIVACY_INDICATOR, 1,
-            UserHandle.USER_CURRENT) == 1;
+    private static boolean shouldShowIndicators() {
+        return DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
+                PROPERTY_CAMERA_MIC_ICONS_ENABLED, true) || shouldShowPermissionsHub();
     }
 
     private boolean shouldShowLocationIndicator() {
@@ -278,15 +275,12 @@ public class PermissionUsageHelper implements AppOpsManager.OnOpActiveChangedLis
             return usages;
         }
 
-        List<String> ops = new ArrayList<>();
-        if (shouldShowCameraIndicator()) {
-            ops.addAll(CAMERA_OPS);
-            if (!isMicMuted) {
-                ops.addAll(MIC_OPS);
-            }
-        }
+        List<String> ops = new ArrayList<>(CAMERA_OPS);
         if (shouldShowLocationIndicator()) {
             ops.addAll(LOCATION_OPS);
+        }
+        if (!isMicMuted) {
+            ops.addAll(MIC_OPS);
         }
 
         Map<String, List<OpUsage>> rawUsages = getOpUsages(ops);
