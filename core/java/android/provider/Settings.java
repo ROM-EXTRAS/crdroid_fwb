7861840582e9a21eb2558d9e184ed17f168988de
SagarMakhar <sagarmakhar@gmail.com>
2022-11-25 00:57:31 +0530

    SystemUI: Black theme [1/3]
    Change-Id: Ib165e7ba0288eaad7a1e3c1109638acdb66fa05c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11014,6 +11014,12 @@ public final class Settings {
         public static final String ENABLE_LOCATION_PRIVACY_INDICATOR = "enable_location_privacy_indicator";
 
         /**
+         * Whether to turn on black theme
+         * @hide
+         */
+        public static final String SYSTEM_BLACK_THEME = "system_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
