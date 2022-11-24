d2261f5d9bc12c73e6ce78b09d1ea734135a5df7
0xD34D <clark@scheffsblend.com>
2022-11-25 00:56:45 +0530

    SQUASH: GestureAnywhere [1/3]
    Change-Id: Ie15c4939af7cfc6ed5dbca5addadffc0b4bfb764
    
    Missing from Gesture Anywhere commit
    
    Adapted to Oreo by @premaca
    
    @Aston-Martinn | #Rebellion-OS - Forward Port to Q
    
    Statusbar is slightly changed and needed bit rework in that area.
    
    Change-Id: I684595fd01e518857071cd0402ded7e125fa869b
    Signed-off-by: Prema Chand Alugu <premaca@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: varund7726 <date.varun123@gmail.com>
    
    GestureAnywhere: Increase gesture anywhere width to 40px [1/2]
    
    Users keep on complaining that Geature Anywhere doesn't work but is basically most don't read (big surprise)
    and don't know they can adjust the width.
    
    Change-Id: Ibbd00c780aaaffb4b9c040cb47ebe4db7f611a6c
    
    GestureAnywhere: Fix GA fc and saving the gesture cache
    
    Change-Id: I01d4bce02dc6b8454c6267f463519183c70f512c
    
    GestureAnywhere: Materiallize gesture anywhere layout
    - Added close fab button
    - Added 3 colors gradient layout background
    - overall improved UI
    - Exposed all colors for the themers
    
    Screenshot : https://drive.google.com/file/d/0B-nhdyBCbCSxajdkR1dyWWZBMGs/view?usp=sharing
    
    Change-Id: Ibbb2cee2d66f79455c5374be124ed13c27a0f691
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: varund7726 <date.varun123@gmail.com>
    
    GestureAnywhere: Layout improvements & switch to dark material theme
    
    - Updated gesture icon
    
    Screenshot: https://drive.google.com/file/d/0B-nhdyBCbCSxc05Ia2N3NzE0UlU/view?usp=sharing
    Change-Id: I75a110ba9bc0ac6d9d9973113d2c2d5823df8ddd
    
    GestureAnywhere: Add missing dimens
    
    Change-Id: I3bb6dc7210ee2adf1b10c6d0658c8bcde6497f49
    
    GestureAnywhere: Set stroke color to device accent
    
    Change-Id: I27e67c9bd8b7f88c3740388b855c90e50a07a02e
    
    GestureAnywhere: Actually use system accent color
    
    GestureAnywhere: Add QuickSettings Tile
    
    Change-Id: I662c8d7651d10045ec0310953add49f5e0a46cc1
    
    GestureAnywhere: Fixup
    
    Change-Id: Iaf277123dd8022a4c4c581590912191ae184de47
    
    GestureAnywhere: Do not clear gesture after drawing
    
    Change-Id: I13e94f0d64d8015ec1a0da9f63a23db1a86a66e4
    
    GestureAnywhere: Fix provider
    
    Change-Id: If62e6e08553e45d19ef3ff88ca9363727bd8ac9f
    
    Co-authored-by: DroidFreak32 <rushabshah32@gmail.com>
    Change-Id: If24a8885fdb8ad85e25aaa4770c84cc7341d7f87

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -41,7 +41,9 @@ import static com.android.systemui.statusbar.phone.BarTransitions.MODE_SEMI_TRAN
 import static com.android.systemui.statusbar.phone.BarTransitions.MODE_TRANSPARENT;
 import static com.android.systemui.statusbar.phone.BarTransitions.TransitionMode;
 
+import android.animation.TimeInterpolator;
 import android.annotation.Nullable;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.ActivityTaskManager;
@@ -71,6 +73,7 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.graphics.PixelFormat;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.hardware.display.DisplayManager;
@@ -99,6 +102,7 @@ import android.util.Log;
 import android.util.MathUtils;
 import android.util.Slog;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.HapticFeedbackConstants;
 import android.view.IRemoteAnimationRunner;
 import android.view.IWindowManager;
@@ -109,6 +113,7 @@ import android.view.ThreadedRenderer;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.view.WindowInsetsController.Appearance;
 import android.view.WindowManager;
 import android.view.WindowManagerGlobal;
@@ -147,6 +152,7 @@ import com.android.systemui.assist.AssistManager;
 import com.android.systemui.biometrics.AuthRippleController;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.camera.CameraIntents;
+import com.android.systemui.chaos.lab.gestureanywhere.GestureAnywhereView;
 import com.android.systemui.charging.WirelessChargingAnimation;
 import com.android.systemui.classifier.FalsingCollector;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
@@ -718,6 +724,7 @@ public class StatusBar extends SystemUI implements
     private final ColorExtractor.OnColorsChangedListener mOnColorsChangedListener =
             (extractor, which) -> updateTheme();
 
+    private boolean mGaEnabled;
 
     /**
      * Public constructor for StatusBar.
@@ -1244,6 +1251,8 @@ public class StatusBar extends SystemUI implements
         mNotificationPanelViewController.setHeadsUpManager(mHeadsUpManager);
 
         createNavigationBar(result);
+
+        addGestureAnywhereView();
 
         if (ENABLE_LOCKSCREEN_WALLPAPER && mWallpaperSupported) {
             mLockscreenWallpaper = mLockscreenWallpaperLazy.get();
@@ -2049,21 +2058,28 @@ public class StatusBar extends SystemUI implements
 
         void observe() {
             ContentResolver resolver = mContext.getContentResolver();
-            /*resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.XXX),
-                    false, this, UserHandle.USER_ALL);*/
+            resolver.registerContentObserver(LineageSettings.System.getUriFor(
+                    LineageSettings.System.GESTURE_ANYWHERE_ENABLED),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            /*if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.XXX))) {
-                doXXX();
-            }*/
         }
 
         public void update() {
-            //doXXX();
+            refreshGA();
+        }
+    }
+
+    private void refreshGA() {
+        mGaEnabled = LineageSettings.System.getInt(
+                    mContext.getContentResolver(), LineageSettings.System.GESTURE_ANYWHERE_ENABLED, 0) == 1;
+        if (mGaEnabled) {
+            LineageSettings.System.putInt(mContext.getContentResolver(),
+                LineageSettings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 1);
+            LineageSettings.System.putInt(mContext.getContentResolver(),
+                LineageSettings.System.GESTURE_ANYWHERE_SHOW_TRIGGER, 0);
         }
     }
 
@@ -4082,6 +4098,8 @@ public class StatusBar extends SystemUI implements
 
     private final Lazy<AssistManager> mAssistManagerLazy;
 
+    protected GestureAnywhereView mGestureAnywhereView;
+
     public boolean isDeviceInteractive() {
         return mDeviceInteractive;
     }
@@ -4699,4 +4717,35 @@ public class StatusBar extends SystemUI implements
                     return mStartingSurfaceOptional.get().getBackgroundColor(task);
                 }
             };
+
+    protected void addGestureAnywhereView() {
+        mGestureAnywhereView = (GestureAnywhereView)View.inflate(
+                mContext, R.layout.gesture_anywhere_overlay, null);
+        mWindowManager.addView(mGestureAnywhereView, getGestureAnywhereViewLayoutParams(Gravity.LEFT));
+        mGestureAnywhereView.setStatusBar(this);
+    }
+
+    protected void removeGestureAnywhereView() {
+        if (mGestureAnywhereView != null)
+            mWindowManager.removeView(mGestureAnywhereView);
+    }
+
+    protected WindowManager.LayoutParams getGestureAnywhereViewLayoutParams(int gravity) {
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.TYPE_STATUS_BAR_SUB_PANEL,
+                0
+                | WindowManager.LayoutParams.FLAG_TOUCHABLE_WHEN_WAKING
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
+                PixelFormat.TRANSLUCENT);
+        lp.privateFlags |= WindowManager.LayoutParams.PRIVATE_FLAG_NO_MOVE_ANIMATION;
+        lp.gravity = Gravity.TOP | gravity;
+        lp.setTitle("GestureAnywhereView");
+
+        return lp;
+    }
 }
